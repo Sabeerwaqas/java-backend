e19e2fa526759477bfd245df680b337e81609ae9
sabeer-ivl <sabeer.waqas@indusvalleylabs.com>
2025-11-04 19:02:43 +0500

    chore: add summy data in local db

---
+++ IRCTC/app/src/main/ticket/booking/services/TrainService.java
@@ -0,0 +1,11 @@
+package IRCTC.app.src.main.ticket.booking.services;
+
+import IRCTC.app.src.main.ticket.booking.entities.User;
+
+import java.io.File;
+
+public class TrainService {
+
+
+
+}
--- IRCTC/app/src/main/ticket/booking/services/UserBookingService.java
+++ IRCTC/app/src/main/ticket/booking/services/UserBookingService.java
@@ -2,13 +2,18 @@ package IRCTC.app.src.main.ticket.booking.services;
 
 import IRCTC.app.src.main.ticket.booking.entities.User;
 
-public class trainService {
+import java.io.File;
 
+public class UserBookingService {
     private User user;
+
+    private static final String USERS_PATH = "../localDb/users.json";
 
     public User UserBookingService(User user1){
 
         this.user = user1;
+
+        File users = new File(USERS_PATH);
 
         return user1;
     }
