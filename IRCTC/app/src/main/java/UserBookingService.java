6d88f1f88ad88a0c2a3eaca6fd448dc94c7e4ff0
Sabeerwaqas <sabeerwaqas050@gmail.com>
2025-11-23 23:12:53 +0500

    chore: copy login, signup code from github

--- IRCTC/app/src/main/java/UserBookingService.java
+++ IRCTC/app/src/main/java/UserBookingService.java
@@ -3,11 +3,14 @@ package IRCTC.app.src.main.ticket.booking.services;
 import IRCTC.app.src.main.ticket.booking.entities.User;
 
 import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.List;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import javax.swing.text.html.Option;
 
 public class UserBookingService {
     private User user;
@@ -24,12 +27,24 @@ public class UserBookingService {
 
         File users = new File(USERS_PATH);
 
-        userList = objectMapper.readValue(users, new TypeReference<List<User>>() {
-            @Override
-            public Type getType() {
-                return super.getType();
-            }
-        });
+        userList = objectMapper.readValue(users, new TypeReference<List<User>>() {});
+
     }
 
+    public Boolean loginUser(){
+        Optional<User> foundUser = userList.stream().filter(user1 -> {
+            return user1.getName.equals(user.getName()) && UserServiceUtil.checkPassword(user.getPassword(), user1.getHashedPassword());
+        }).findFirst();
+        return foundUser.isPresent();
+    }
+
+    public Boolean signUp(User user1){
+        try{
+            userList.add(user1);
+            saveUserListToFile();
+            return Boolean.TRUE;
+        }catch (IOException ex){
+            return Boolean.FALSE;
+        }
+    }
 }
