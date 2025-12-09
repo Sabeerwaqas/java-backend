724f11124e09c5d41b47dddc95653dfbc123895f
sabeer-ivl <sabeer.waqas@indusvalleylabs.com>
2025-12-09 14:35:34 +0500

    chore: learn about method reference

--- java/src/functionalInterface/FunctionalInterfaceDemo.java
+++ java/src/functionalInterface/FunctionalInterfaceDemo.java
@@ -1,5 +1,6 @@
 package functionalInterface;
 
+import java.lang.classfile.Interfaces;
 import java.util.function.Function;
 
 @FunctionalInterface
@@ -9,11 +10,15 @@ interface BookAction {
 
 public class FunctionalInterfaceDemo {
     public static Function<Integer, Integer> addFunction = (a) -> a+3;
+    public static Function<Integer, Integer> subtractFunction = (a) -> a - 3;
+    public static Function<Integer, Integer> combinedFunction = addFunction.andThen(subtractFunction);
 
     static void main() {
         BookAction action = () -> System.out.println("Perform");
 
         System.out.println(addFunction.apply(3));
+        System.out.println(subtractFunction.apply(3));
+        System.out.print(combinedFunction.apply(3));
     }
 
 }
