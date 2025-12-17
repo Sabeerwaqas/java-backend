fbf8031cd0c3c6c040f722166fc906f6908c0e5e
sabeer-ivl <sabeer.waqas@indusvalleylabs.com>
2025-12-17 16:47:33 +0500

    chore: get input from user

--- book-store-challenge/src/app/Main.java
+++ book-store-challenge/src/app/Main.java
@@ -2,8 +2,15 @@ package app;
 
 import service.bookService.BookService;
 
+import java.util.Scanner;
+
 public class Main {
     static void main() {
+
+        Scanner scanner = new Scanner(System.in);
+        int selectedNum = scanner.nextInt();
+
+        System.out.println(selectedNum);
 
         BookService bookService = new BookService();
         bookService.getAllBooks();
