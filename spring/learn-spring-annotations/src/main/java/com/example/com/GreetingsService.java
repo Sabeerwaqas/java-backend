69282b320d4ca29fd750c5e23aab5bdcff35c613
Sabeerwaqas <sabeerwaqas050@gmail.com>
2025-12-23 23:38:48 +0500

    chore: learn about annotations

--- spring/learn-spring-annotations/src/main/java/com/example/com/GreetingsService.java
+++ spring/learn-spring-annotations/src/main/java/com/example/com/GreetingsService.java
@@ -1,5 +1,8 @@
 package com.example.com;
 
+import org.springframework.stereotype.Component;
+
+@Component("myBean")
 public class GreetingsService {
     public void sayHello(){
         System.out.println("Hello From Spring!");
--- spring/learn-spring-annotations/src/main/java/com/example/com/Main.java
+++ spring/learn-spring-annotations/src/main/java/com/example/com/Main.java
@@ -1,11 +1,12 @@
 package com.example.com;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("applicationBeanContext.xml");
+        ApplicationContext context = new AnnotationConfigApplicationContext(NewAppConfig.class);
         com.example.com.GreetingsService greetingsService = (com.example.com.GreetingsService) context.getBean("myBean");
         greetingsService.sayHello();
     }
