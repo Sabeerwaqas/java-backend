1b2b9024819688a0f734c15eca2b79c4610d4ffa
sabeer-ivl <sabeer.waqas@indusvalleylabs.com>
2025-12-31 18:06:25 +0500

    chore: create CRUD APIs

--- spring/demo/src/main/java/com/demo/first/app/UserController.java
+++ spring/demo/src/main/java/com/demo/first/app/UserController.java
@@ -1,11 +1,10 @@
 package com.demo.first.app;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -15,7 +14,25 @@ public class UserController {
 
     @PostMapping
     public String createUser(@RequestBody User user){
-        userDb.put(user.getId(), user);
+        userDb.putIfAbsent(user.getId(), user);
         return "User Created";
+    }
+
+    @PutMapping
+    public String updateUser(@RequestBody User user){
+        if(userDb.containsKey(user.getId())){
+            userDb.put(user.getId(), user);
+        }
+        return "User Updated Successfully!";
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteUser(@PathVariable int id){
+        userDb.remove(id);
+        return "User deleted successfully";
+    }
+
+    public List<User> getUsers(){
+        return new ArrayList<>(userDb.values());
     }
 }
